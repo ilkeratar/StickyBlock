11599f515202b9e8cf227ab32cc566e7a69de8d3
İlker Atar <74013091+ilkeratar@users.noreply.github.com>
2022-01-17 17:56:52 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Material collectedObjMat;
+    public PlayerState playerState;
+    public LevelState levelState;
+    
+    public Transform particlePrefab;
+
+    public List<GameObject> collidedList;
+    public Transform collectedPoolTransform;
+    public enum PlayerState{
+        Stop,
+        Move
+    }
+    public enum LevelState{
+        NotFinished,
+        Finished
+    }
+
+    public void CallMakeSphere(){
+        foreach (GameObject obj in collidedList)
+        {
+            obj.GetComponent<CollectedObjController>().MakeSphere();
+        }
+    }
+}
